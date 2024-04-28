d4bda7197ff59b7feeed6449e331575b6f61e55d
Echo-G <120436863+Echo-G@users.noreply.github.com>
2024-04-28 23:38:39 +0800

    AI太厉害了
    以一个我绝对想不到的解决方法解决了这个问题，现在Power值已经比较成功了

--- ofriend.cs
+++ ofriend.cs
@@ -1,44 +1,41 @@
 using System;
 using Terraria.Audio;
 using Terraria.ModLoader;
-
 namespace ofriend
 {
     public class ofriend : Mod
     {
-
     }
+
     public class Power
     {
         public static int NowPower { get; private set; }
         public static int MaxPower { get; } = 4000;
-        private bool soundPlay;
-        public void SoundPowerup()
+
+        public static void PlayPowerupSound()
         {
-            if (NowPower == 1000 || NowPower == 2000 || NowPower == 3000)
-            {
-                SoundStyle soundpowerup = new SoundStyle("ofriend/Assets/Sounds/Powerup");
-                SoundEngine.PlaySound(soundpowerup);
-            }
-            if (NowPower == 4000 && soundPlay == false)
-            {
-                SoundStyle soundpowerup = new SoundStyle("ofriend/Assets/Sounds/Powerup");
-                SoundEngine.PlaySound(soundpowerup);
-                soundPlay = true;
-            }
+            SoundStyle soundpowerup = new SoundStyle("ofriend/Assets/Sounds/Powerup");
+            SoundEngine.PlaySound(soundpowerup);
         }
 
         public static void IncreasePower(int amount)
         {
+            int previousPower = NowPower;
             NowPower = Math.Min(NowPower + amount, MaxPower);
-            Power power = new Power();
-            power.SoundPowerup();
+
+            // 检查NowPower是否跨过了1000、2000、3000或4000的阈值
+            if ((previousPower < 1000 && NowPower >= 1000) ||
+                (previousPower < 2000 && NowPower >= 2000) ||
+                (previousPower < 3000 && NowPower >= 3000) ||
+                (previousPower < MaxPower && NowPower >= MaxPower))
+            {
+                PlayPowerupSound();
+            }
         }
+
         public static void ResetPower()
         {
             NowPower = 0;
-            Power power = new Power();
-            power.soundPlay = false;
         }
     }
 }
