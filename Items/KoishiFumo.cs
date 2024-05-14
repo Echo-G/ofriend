139da99146840d99750f011fd86146d8c1b66b09
Echo-G <120436863+Echo-G@users.noreply.github.com>
2024-05-14 23:42:45 +0800

    add Koishi Fumo &update .net8.0
    fumofumo~

---
+++ Items/KoishiFumo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace ofriend.Items
+{
+    public class KoishiFumo:ModItem
+    {
+        public override void SetDefaults()
+        {
+            // Vanilla has many useful methods like these, use them! This substitutes setting Item.createTile and Item.placeStyle aswell as setting a few values that are common across all placeable items
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.KoishiFumoTile>());
+            Item.width = 18;
+            Item.height = 19;
+            Item.maxStack = 999;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.buyPrice(0, 1);
+        }
+    }
+}
---
+++ Tiles/KoishiFumoTile.cs
@@ -0,0 +1,41 @@
+﻿using ofriend.Items;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+namespace ofriend.Tiles
+{
+    public class KoishiFumoTile:ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileID.Sets.FramesOnKillWall[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.newTile.Width = 5;
+            TileObjectData.newTile.Height = 5;
+            TileObjectData.newTile.CoordinateHeights = new int[] { 16, 16, 16, 16, 16 };
+            TileObjectData.addTile(Type);
+            AddMapEntry(new Color(120, 65, 60), Language.GetText("KoishiFumo"));
+            DustType = 7;
+        }
+        public override bool Slope(int i, int j)
+        {
+            return false;
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 32, ModContent.ItemType<KoishiFumo>());
+        }
+    }
+}
